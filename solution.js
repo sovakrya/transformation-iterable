6bf9de816a574d1a7235da278d6cfac16ce79f85
sovaKrya <tyan_34ee@mail.ru>
2024-09-19 16:44:48 +0300

    corrected solution

--- solution.js
+++ solution.js
@@ -1,29 +1,5 @@
-export function transformInArray(obj) {
-  const res = [];
+export const transformInArray = (obj) =>
+  Object.entries(obj).map(([key, value]) => ({ name: key, value }));
 
-  for (let key in obj) {
-    const newObj = {
-      name: "",
-      value: "",
-    };
-    newObj.name = key;
-    newObj.value = obj[key];
-    res.push(newObj);
-  }
-
-  return res;
-}
-
-
-export function transformInObject(arr) {
-  const res = {};
-
-  for (let val of arr) {
-    res[val.name] = val.value
-  }
-
-  return res;
-}
-
-
-
+export const transformInObject = (arr) =>
+  Object.fromEntries(arr.map((obj) => [obj.name, obj.value]));
